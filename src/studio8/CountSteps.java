e3439e2a406f40de2f8e2bc317654377d3007eaf
mitchellblack <mitchellblack@mitchells-mbp.dhcp.wustl.edu>
2018-03-19 14:27:35 -0500

    studio 8

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -1,7 +1,31 @@
 package studio8;
 
-// TODO: Develop an algorithm to count steps in accelerometer data
+import java.util.Scanner;
+import java.io.*;
+
+
+
+// TODO: Develop an algorithm to count steps in accelerometer dat
 //    Major steeps:
 //       1. Create a class and main method.
 //       2. Using a Scanner and File object, read data from your .csv file.
 //       3. Develop and test algorithms to count the "peaks" in the data.
+
+public class CountSteps{
+	public static void main(String args[]) {
+		try {
+		
+		File txt =   new File("/Users/mitchellblack/Documents/Wash U SP18/CSE 132/item.csv");
+		Scanner in = new Scanner(txt);
+		
+		if(txt.canRead()) {
+			String line = in.nextLine();
+			System.out.println(line);
+		}
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
